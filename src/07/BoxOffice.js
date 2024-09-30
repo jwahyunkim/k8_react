d19557538681009d0f3adf20bf2f6dd1700c0e4c
jwahyunkim <largoing@gmail.com>
2024-09-30 10:38:06 +0900

    박스오피스

--- src/07/BoxOffice.js
+++ src/07/BoxOffice.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from "react"
 export default function BoxOffice() {
+
+    //맨처음 한번 실행
+    useEffect(() => {
+        const apiKey = process.env.REACT_APP_MV_KEY
+        console.log('apikey=', apiKey)
+    }, []);
+
+
     return (
-        <div>
-            BoxOffice
+        <div className="h-screen flex justify-center items-center">
+            <div>
+
+            </div>
         </div>
     )
 }
---
+++ src/07/BoxOffice.useEffect.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react"
+import TailButton from '../UI/TailButton';
+
+export default function BoxOffice() {
+    const [cnt, setCnt] = useState();
+
+    const handleUp = () => {
+        setCnt(cnt + 1);
+
+
+    }
+    //맨처음 한번 실행
+    useEffect(() => {
+        console.log('useEffect[]');
+        setCnt(100);
+    }, []);
+
+
+    //state 변수 cnt가 변경이 될때
+    useEffect(() => {
+        console.log('useEffect[cnt]', cnt);
+
+    }, [cnt]);
+
+    //변경이 일어 날때마다 실행
+    useEffect(() => {
+        console.log('useEffect[cnt]');
+
+    }, [cnt]);
+
+    return (
+        <div className="h-screen flex justify-center items-center">
+            <div className="flex justify-center itmes-center m-5
+                            text-3xl">
+                {cnt}
+            </div>
+            <div>
+                <TailButton caption='증가'
+                    color='blue'
+                    handleClick={handleUp} />
+            </div>
+        </div >
+    )
+}
